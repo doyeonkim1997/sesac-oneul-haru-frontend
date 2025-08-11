c1a0ceab7f9822326356f3bfbcad5f384450c6d8
MA-Ha-eun <1thecalla@gmail.com>
2025-08-12 04:15:40 +0900

    feat(RootRedirector 외): 사용자 인증 아키텍처 개선, localStorage 통한 진입 경로 처리 추가, 관련 코드 리팩토링, 콘솔 에러 해결

--- src/api/axiosInstance.ts
+++ src/api/axiosInstance.ts
@@ -1,51 +1,38 @@
 import axios, { AxiosError, type AxiosRequestConfig } from 'axios';
 
 let accessToken: string | null = null;
-
 let imageUrl: string | null = null;
-
 let nickName: string | null = null;
-
 let tier: string | null = null;
-
 let authType: string | null = null;
 
 export function setAccessToken(token: string | null): void {
   accessToken = token;
 }
-
 export function getAccessToken(): string | null {
   return accessToken;
 }
-
 export function setImageUrl(image: string | null): void {
   imageUrl = image;
 }
-
 export function getImageUrl(): string | null {
   return imageUrl;
 }
-
 export function setNickName(name: string | null): void {
   nickName = name;
 }
-
 export function getNickName(): string | null {
   return nickName;
 }
-
 export function setTier(userTier: string | null): void {
   tier = userTier;
 }
-
 export function getTier(): string | null {
   return tier;
 }
-
 export function setAuthType(type: string | null): void {
   authType = type;
 }
-
 export function getAuthType(): string | null {
   return authType;
 }
@@ -54,13 +41,11 @@ interface CustomAxiosRequestConfig extends AxiosRequestConfig {
   _retry?: boolean;
 }
 
-// axios 인스턴스 생성
 const instance = axios.create({
   baseURL: import.meta.env.VITE_BACKEND_ADDRESS,
-  withCredentials: true, // 쿠키 전송 허용
+  withCredentials: true,
 });
 
-// 요청 인터셉터: 요청 시 토큰 자동 삽입
 instance.interceptors.request.use((config) => {
   const token = getAccessToken();
   if (token) {
@@ -70,11 +55,15 @@ instance.interceptors.request.use((config) => {
   return config;
 });
 
-// 응답 인터셉터: 401일 때 토큰 재발급 시도
 instance.interceptors.response.use(
   (response) => response,
   async (error: AxiosError) => {
     const originalRequest = error.config as CustomAxiosRequestConfig;
+
+    // 수정
+    if (originalRequest.url === '/auth/refresh') {
+      return Promise.reject(error);
+    }
 
     if (error.response?.status === 401 && !originalRequest._retry) {
       originalRequest._retry = true;
@@ -84,7 +73,6 @@ instance.interceptors.response.use(
           {},
           { withCredentials: true },
         );
-
         const { accessToken: newToken, nickName, imageUrl, tier, authType } = res.data;
 
         setAccessToken(newToken);
@@ -96,12 +84,13 @@ instance.interceptors.response.use(
         if (originalRequest.headers) {
           originalRequest.headers.Authorization = `Bearer ${newToken}`;
         }
-        return instance(originalRequest); // 재요청
+
+        return instance(originalRequest);
       } catch (refreshError) {
-        // 리프레시 실패 시 로그아웃 처리 or 에러 반환
         return Promise.reject(refreshError);
       }
     }
+    
     return Promise.reject(error);
   },
 );
